1c9f99d244987990d629e5e4f88de9bf4ca93025
sandeepanumalla <anumalla.sandeep1999@gmail.com>
2023-04-10 01:24:55 +0530

    first commit

---
+++ src/test/java/com/panorbit/searchapp/SearchappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panorbit.searchapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
